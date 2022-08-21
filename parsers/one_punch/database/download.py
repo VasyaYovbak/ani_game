d25e5be2a39c46ce929d78e9c816c30c10464243
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-08-21 12:53:38 +0300

    Prepared one punch man data for downloading into database.

--- parsers/one_punch/database/download.py
+++ parsers/one_punch/database/download.py
@@ -1,1 +1,14 @@
-# download you data to database here =)
+from parsers.functions.setup import *
+
+# preparing data
+
+df = pd.read_csv('../data/one_punch.txt')
+
+df.rename(columns={'full_name': 'name'}, inplace=True)
+df = df.assign(anime_id=lambda x: 2)
+number_of_img = 137
+one_img_size = 150
+
+df.drop(columns=['image'], inplace=True)
+
+df = df.assign(image_x=[x for x in range(0, number_of_img * one_img_size + 1, one_img_size)])

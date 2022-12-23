388a6044e76f46a2c90568b9e4a93baa6c2881c2
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-12-23 21:23:04 +0200

    Updated Redis connection.

--- user_view/redis_connection.py
+++ user_view/redis_connection.py
@@ -4,7 +4,7 @@ import os
 # client = redis.Redis(host='localhost', port=6379, db=0)
 r = redis.Redis(
   host=os.getenv('host'),
-  port=os.getenv('port'),
+  port=int(os.getenv('port')),
   password=os.getenv('password')
 )
 

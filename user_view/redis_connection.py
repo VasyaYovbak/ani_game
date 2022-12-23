d402c3fc85aafd09e77f5d7c27c49c038acac1a0
RomanOstrovskiy <romaostrovskiy616@gmail.com>
2022-12-23 20:43:00 +0200

    Fixed validation, added redis cloud, fixed logout logic, additional methods from view.py replaced to additional_methods.py,

--- user_view/redis_connection.py
+++ user_view/redis_connection.py
@@ -1,17 +1,13 @@
 import redis
-# from datetime import timedelta, datetime, timezone
-#
-jwt_redis_blacklist = redis.Redis(
-    host="localhost", port=6379, db=0, decode_responses=True
+import os
+
+# client = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(
+  host=os.getenv('host'),
+  port=os.getenv('port'),
+  password=os.getenv('password')
 )
-# jti = 'sdawdasdawdasdawd'
-# ACCESS_EXPIRES = timedelta(days=1)
-#
-#
-# print(jwt_redis_blacklist.set(name="jti", value=jti, ex=ACCESS_EXPIRES))
-# print(str(jwt_redis_blacklist.get('jti')))
-#
-# access_token_lifetime = datetime.fromtimestamp(1660929512)
-# print(access_token_lifetime)
-#
-# jwt_redis_blacklist.close()
+
+
+
+

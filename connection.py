0245f35a7d35651bf0f4d779bcaea6c34b763c0a
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-12-24 16:09:36 +0200

    Modified connection.py

--- connection.py
+++ connection.py
@@ -1,17 +1,20 @@
+import os
+
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import Session
 from config import USERNAME, PASSWORD, SERVER, DB
 
 
-metadata = MetaData()
-engine = create_engine(f"mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}")
-session = Session(bind=engine)
-
-
-# from sqlalchemy import create_engine, MetaData
-# from sqlalchemy.orm import Session
-#
 # metadata = MetaData()
-# engine = create_engine(
-#     "ibm_db_sa://xnr13607:ov46GXDz5oVhewv5@2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud:32328/bludb?security=SSL")
+# engine = create_engine(f"mysql+pymysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}")
 # session = Session(bind=engine)
+
+
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import Session
+
+CONNECTION_URL = os.getenv("CONNECTION_URL")
+
+metadata = MetaData()
+engine = create_engine(CONNECTION_URL)
+session = Session(bind=engine)

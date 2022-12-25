f44938954c443316d60c1203e76f9e89938627d9
nazarkohut <85364294+nazarkohut@users.noreply.github.com>
2022-12-25 13:06:02 +0200

    Refactored fields validation error messages.

--- user_view/validation.py
+++ user_view/validation.py
@@ -2,44 +2,41 @@ from marshmallow.validate import *
 
 
 def password_check(password):
-
-    Special_Symbols = ['$', '@', '#', '%', '/']
+    special_symbols = {'$', '@', '#', '%', '/'}
 
     if len(password) <= 5 or len(password) >= 37:
-        raise ValidationError('Password length should be between 6 and 36 digits')
+        raise ValidationError('Password length should be between 6 and 36 digits.')
 
     if not any(char.isdigit() for char in password):
-        raise ValidationError('Password should have at least one of the symbols: $, @, #, %, /')
+        raise ValidationError('Password should contain at least one digit.')
+
+    if not any(char in special_symbols for char in password):
+        raise ValidationError(
+            f"Password should have at least one of the symbols: {get_special_symbols_repr(special_symbols)}")
 
 
 # check if username have digits
 def username_check(username):
-
-    Special_Symbols = {'$', '@', '#', '%', '/', '^', '~', }
-
-
     special_symbols = {'$', '@', '#', '%', '/', '^', '~', }
-
 
     ans = any(char.isdigit() for char in username)
 
     if len(username) <= 2 or len(username) >= 33:
-        raise ValidationError('Username length should be between 3 and 32 digits')
+        raise ValidationError('Username length should be between 3 and 32 digits.')
 
     if ans:
-        raise ValidationError("Username should have only letters, digits are not allowed.")
+        raise ValidationError("Username should contain only letters. Digits are not allowed!")
 
     if any(char in special_symbols for char in username):
-        raise ValidationError('This symbols are not allowed.')
+        raise ValidationError(f"{get_special_symbols_repr(special_symbols)} symbols are not allowed.")
 
 
 def email_check(email):
-
     if len(email) <= 7 or len(email) >= 346:
-        raise ValidationError('Email length should be between 8 and 345 digits')
+        raise ValidationError('Email length should be between 8 and 345 digits.')
 
     if email.count('@') != 1:
-        raise ValidationError('Email is not valid')
+        raise ValidationError('Email is not valid.')
 
 
 def validate_registration(username, password, email):
@@ -49,6 +46,9 @@ def validate_registration(username, password, email):
 def validate_password(password):
     return password_check(password)
 
+
+def get_special_symbols_repr(special_symbols):
+    return ', '.join(list(special_symbols))
 
 # class AchievementSchema(Schema):
 #
